7e0a590bf2ca28dab4927d3b39b3f17f809bd86a
Muhammad Ahkam <muhammad.ahkam@golootlo.pk>
2023-10-17 19:33:23 +0500

    Added Test cases

--- app/src/test/java/com/ahkam/mediassist/ExampleUnitTest.kt
+++ app/src/test/java/com/ahkam/mediassist/ExampleUnitTest.kt
@@ -1,17 +1,37 @@
 package com.ahkam.mediassist
 
+
+
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.ahkam.mediassist.presentation.composables.LoginScreen
+import com.ahkam.mediassist.presentation.composables.SplashScreen
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+
 class ExampleUnitTest {
+    @Rule
+    @JvmField
+    var composeTestRule: ComposeContentTestRule = createComposeRule()
+
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun whenIClickOnButton_TheTextShouldChange() {
+        composeTestRule.onNodeWithText("Hello").assertExists()
+        composeTestRule.onNodeWithText("Hello").performClick()
+        composeTestRule.onNodeWithText("Hello").assertDoesNotExist()
+        composeTestRule.onNodeWithText("Bye").assertExists()
     }
+
 }
